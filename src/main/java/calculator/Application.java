8bdf76dda5e79a2b17d616455a5608f7344e92d9
anmin <iededg@naver.com>
2025-10-20 17:21:07 +0900

    feat: 콘솔 입력 및 결과 출력 메인 클래스 구현

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,16 @@
 package calculator;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class Application {
+
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        try {
+            String input = Console.readLine();
+            int result = StringCalculator.calculate(input);
+            System.out.println("결과 : " + result);
+        } catch (IllegalArgumentException e) {
+            throw e; // 테스트에서 예외 검증하므로 다시 던짐
+        }
     }
 }
